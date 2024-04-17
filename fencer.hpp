a990b5cb09f71d78edfa0b6e03cd1ffca32ae90c
František Vokáč <vokacfra@fit.cvut.cz>
2024-04-17 19:50:55 +0200

    structure for fencer

--- fencer.hpp
+++ fencer.hpp
@@ -11,9 +11,36 @@ class CFencer : public CPerson
              const int birthYear,
              const int globalRank,
              const int id);
+    std::string getWeapon () const;
+    int getGlobalRank () const;
+    int getPoolSeed () const;
+    int getDESeed () const;
+    int getWonPoolMatches () const;
+    int getLostPoolMatches () const;
+    float getWonPoolMatchesRatio () const;
+    int getScoredPoolTouches () const;
+    int getReceivedPoolTouches () const;
+    int getPoolTouchesIndex () const;
+    int getId () const;
+    
+    void setPoolSeed (int seed);
+    void setDESeed (int seed);
+    void setWonPoolMatches (int matches);
+    void setLostPoolMatches (int matches);
+    void setScoredPoolTouches (int touches);
+    void setReceivedPoolTouches (int touches);
+    void setPoolTouchesIndex (int index);
+    
+    void calculatePoolMatchesRatio ();
+    void calculatePoolTouchesIndex ();
+
+
+
+    bool isLeftHanded;
+    bool recievedG3RedCard;
+    bool recievedBlackCard;
   private:
     std::string m_weapon;
-    bool m_isLeftHanded;
     int m_birthYear;
     int m_globalRank;
     int m_poolSeed;
@@ -24,5 +51,5 @@ class CFencer : public CPerson
     int m_scoredPoolTouches;
     int m_receivedPoolTouches;
     int m_poolTouchesIndex;
-    int m_id;    
+    int m_id;
 };
