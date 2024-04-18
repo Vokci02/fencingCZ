ddde2dc2d0ce381876e68adbab5a41e3a85057ee
František Vokáč <vokacfra@fit.cvut.cz>
2024-04-18 10:08:23 +0200

    basic fencer test

--- test.cpp
+++ test.cpp
@@ -1,4 +1,5 @@
 #include "referee.hpp"
+#include "fencer.hpp"
 
 int main ()
 {
@@ -14,6 +15,24 @@ int main ()
     r2.addFinalMatchRefered ();
   r1.print ();
   r2.print ();
+  
+  CFencer f1 ( "Petr", "Novak", "TJLKV", "CZE", "epee", true, 2000, 1, 1 );
+  CFencer f2 ( "Jiri", "Svoboda", "TJLKV", "CZE", "foil", false, 1999, 2, 2 );
+  f1.setWonPoolMatches ( 5 );
+  f1.setLostPoolMatches ( 1 );
+  f2.setWonPoolMatches ( 3 );
+  f2.setLostPoolMatches ( 3 );
+  f1.setScoredPoolTouches ( 30 );
+  f1.setReceivedPoolTouches ( 20 );
+  f2.setScoredPoolTouches ( 25 );
+  f2.setReceivedPoolTouches ( 25 );
+  f1.calculatePoolMatchesRatio ();
+  f2.calculatePoolMatchesRatio ();
+  f1.calculatePoolTouchesIndex ();
+  f2.calculatePoolTouchesIndex ();
+  f1.print ();
+  f2.print ();
+
   return 0;
 
 }
