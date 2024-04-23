817c7496eb39c3386f1a2ba0243f15223ed67bae
František Vokáč <vokacfra@fit.cvut.cz>
2024-04-23 12:02:32 +0200

    add validate score

--- match.hpp
+++ match.hpp
@@ -10,6 +10,7 @@ class CMatch
     virtual int            assignFencers ( const CFencer &fencer1, const CFencer &fencer2 ) = 0        ;
     virtual int            finishMatch   ( int leftScore, int rightScore )                  = 0        ;
     virtual void           repairScore   ( int leftScore, int rightScore )                  = 0        ;
+    virtual int            checkScoreIn  ( int leftScore, int rightScore )                  = 0        ;
     std::pair <int, int>   getScore      ()                                                       const;
     CFencer                getWinner     ()                                                       const;
     void                   setWinner     ( const CFencer &winner );
